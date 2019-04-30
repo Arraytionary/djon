641853488619a4fa831117cf28bfe9ffcdda07d3
Arraytionary <31273055+Arraytionary@users.noreply.github.com>
2019-05-01 01:15:04 +0700

    adding running animation

--- Assets/playerControler.cs
+++ Assets/playerControler.cs
@@ -9,10 +9,12 @@ public class playerControler : MonoBehaviour
     public float velocity;
     private float moveInput;
     public GameObject weapon;
+    private Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -20,6 +22,7 @@ public class playerControler : MonoBehaviour
         moveInput = Input.GetAxisRaw("Horizontal");
         if(moveInput == 1)
         {
+            anim.SetBool("isRunning", true);
             transform.eulerAngles = new Vector3(0, 0, 0);
             weapon.GetComponent<weap>().offset = 0;
             weapon.GetComponent<weap>().rotY = 0;
@@ -28,11 +31,16 @@ public class playerControler : MonoBehaviour
         }
         else if(moveInput == -1)
         {
+            anim.SetBool("isRunning", true);
             transform.eulerAngles = new Vector3(0, 180, 0);
             weapon.GetComponent<weap>().offset = -180;
             weapon.GetComponent<weap>().rotY = 180;
             weapon.GetComponent<weap>().zMul = -1;
             //weapon.transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
         }
         velocity = moveInput * speed;
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
