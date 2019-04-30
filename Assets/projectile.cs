ad809d93308c61a00a1f26a1c3d7816baf6b489a
Arraytionary <31273055+Arraytionary@users.noreply.github.com>
2019-04-30 19:44:41 +0700

    Initial commit

---
+++ Assets/projectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    public GameObject destroyEffect;
+    //private Vector3 travelVal = Vector2.right * speed * Time.deltaTime;
+
+    private void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    void DestroyProjectile()
+    {
+        Debug.Log(transform.position);
+        Debug.Log(transform.up * 9);
+        //Instantiate(destroyEffect, transform.position + Vector3.Scale(transform.position, new Vector3(2f,2f,1f)), Quaternion.identity);
+        Instantiate(destroyEffect, transform.position + transform.right * 1.1f, Quaternion.identity);
+        //Vector3.Scale(transform.position, new Vector3(-2f, 2f, 1f)), Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/weap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float offset;
+    public GameObject projectile;
+    public Transform shotPoint;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+        if (Input.GetMouseButtonDown(0) && timeBtwShots<=0)
+        {
+            Instantiate(projectile, shotPoint.position, transform.rotation);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
