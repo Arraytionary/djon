b29fd2f48a3d23ac32cc1c8d99404acdc6a47c6e
Arraytionary <31273055+Arraytionary@users.noreply.github.com>
2019-04-30 23:22:02 +0700

    add walking left and right
    require walking animation

--- Assets/weap.cs
+++ Assets/weap.cs
@@ -14,13 +14,15 @@ public class weap : MonoBehaviour
     public Transform shotPoint;
     private float timeBtwShots;
     public float startTimeBtwShots;
+    public float rotY;
+    public float zMul;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+        transform.rotation = Quaternion.Euler(0f, rotY, zMul * rotZ + offset);
 
         if (Input.GetMouseButtonDown(0) && timeBtwShots<=0)
         {
