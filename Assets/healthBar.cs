bf9940c93671a38af4571a2a87c82dd5a497e018
Arraytionary <31273055+Arraytionary@users.noreply.github.com>
2019-06-18 00:41:35 +0700

    add health bar

---
+++ Assets/healthBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject bar = gameObject.transform.Find("bar").gameObject;
+        bar.transform.localScale = new Vector3(.4f, 1f);
+    }
+
+    // TODO make a singleton to hold the health and use this update to sync the health bar
+    void Update()
+    {
+        
+    }
+}
